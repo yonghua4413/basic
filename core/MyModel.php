78c9e646a12e8b57d562261006fc4b0db6773d85
yonghua <254274509@qq.com>
2018-07-23 11:01:10 +0800

    添加自定义父类模型

---
+++ core/MyModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yonghua
+ * Date: 2018-07-23
+ * Time: 10:58
+ */
+
+namespace app\core;
+
+
+use yii\db\ActiveRecord;
+
+class MyModel extends ActiveRecord
+{
+
+}
